e4c4fadfa49c53dac2a33dbab9a4cda7b9793e0f
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-04-01 12:17:09 -0700

    added css exercises

---
+++ Coding Practice/apr-01-warmup.js
@@ -0,0 +1,27 @@
+//Write a function that returns the reading status of each of following books
+
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games',
+        readingStatus: false
+    }];
+
+function readingChecker(books) {
+
+    shoppers.forEach(book => {
+            console.log(shopper.name + " initial amount " + shopper.amount);
+
+    });
+}
+calcDiscount(shoppersArray);
