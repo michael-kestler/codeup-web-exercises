e4df486bc04fd178358bab77da7a0d1af2645976
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-04-08 09:11:06 -0700

    added coding practice update

--- Coding Practice/retake-practice.js
+++ Coding Practice/retake-practice.js
@@ -50,27 +50,48 @@ function isVowel(letter){
     }
 }
 
-/* ** the questions with an asterisk * are more challenging
+//the questions with an asterisk * are more challenging//
 
-6. Write a function called first(input) that returns the first character in the provided string.
+//6. Write a function called first(input) that returns the first character in the provided string.//
+function first(input){
+  return input.slice(0, 1);
+}
 
-7* Create a function that takes in two string inputs.
-   -- If the second string input is present in the first, return the first input string with the second input string removed from it.
-   -- If the second string input is present multiple times in the first, the second string will only be removed where it first occurs in the first string.
-   -- If the second string input is not present in the first, return the first string as entered in the function.
 
-8. Make a function named isSpace(letter) that returns if a character is a space character
+// 7* Create a function that takes in two string inputs.
+//    -- If the second string input is present in the first, return the first input string with the second input string removed from it.
+//    -- If the second string input is present multiple times in the first, the second string will only be removed where it first occurs in the first string.
+//    -- If the second string input is not present in the first, return the first string as entered in the function.
 
-9. Write a function named squareRoot(n) that returns the square root of the input
+//create function that takes in two string inputs
+//write an if statement that checks if second string present in first string
+function removeSecond(firstString, secondString){
 
-10* create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
+    if (firstString.includes(secondString)){
+    return firstString.slice(secondString.length)}
+}
+// }
 
-11. Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
+// var firstString = "javascript"
+// var secondString = "script"
+//
+// console.log(firstString.includes(secondString));
 
-12. Write a function named getRandomQuote().
-//   Inside of the function, create an array of strings where each string is a quote or thought you find inspirational
-//   getRandomQuote should generate a random number between 0 and the array's length minus 1
-//   use the randomly generated number as your index
-//   return a random quote.
 
-13. Write a function named firstTenFibonacciNumbers() that returns an array of the first ten fibonacci numbers  */
+// 8. Make a function named isSpace(letter) that returns if a character is a space character
+
+
+
+// 9. Write a function named squareRoot(n) that returns the square root of the input
+//
+// 10* create a function that will return how many whitespace characters are at the beginning of a string (hint: use regex).
+//
+// 11. Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
+//
+// 12. Write a function named getRandomQuote().
+// //   Inside of the function, create an array of strings where each string is a quote or thought you find inspirational
+// //   getRandomQuote should generate a random number between 0 and the array's length minus 1
+// //   use the randomly generated number as your index
+// //   return a random quote.
+//
+// 13. Write a function named firstTenFibonacciNumbers() that returns an array of the first ten fibonacci numbers  */
