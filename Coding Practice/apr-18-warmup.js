fca49de43cde98a9f1bc0f06fbc2239fd00b0b33
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-04-18 18:40:57 -0700

    refactored contact info section of resume

---
+++ Coding Practice/apr-18-warmup.js
@@ -0,0 +1,16 @@
+
+//rewrite the following as a for loop
+var i = 20;
+while (i >= 0) {
+    console.log(i);
+    i--;
+}
+
+
+// 2. Write a function that takes an object (a) as argument and returns an array with all object keys
+//
+// example input: {a:1,b:2,c:3}
+// expected output: ['a','b','c']
+// ------------------------
+//     example input:{j:9,i:2,x:3,z:4}
+// expected output: ['j','i','x','z']
