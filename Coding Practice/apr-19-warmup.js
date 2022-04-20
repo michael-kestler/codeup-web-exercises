4bde2ca608cf3bc388790dc87bca64a3903c649b
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-04-20 10:08:53 -0700

    added several practice files

---
+++ Coding Practice/apr-19-warmup.js
@@ -0,0 +1,10 @@
+//Write a function that iterates through numbers 1 and 15 and returns the numbers that are divisible by 3 (hint hint hint: learn how to use a modulus)
+
+//Write a function that takes a Set and an array as arguments. If not already existing, add each element in the array to the Set. Return the modified Set
+//
+// example input: new Set([1, 2, 3]), [4, 5, 6]
+// expected output: new Set([1, 2, 3, 4, 5, 6 ])
+// -------------------------
+// example input:new Set([1, 2, 3]), [2, 3]
+// expected output: new Set([1, 2, 3])
+// -------------------------
