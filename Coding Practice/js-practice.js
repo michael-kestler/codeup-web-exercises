1d077d0dd3cd42c7c6d5182ae0a46a7c01c402e8
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-04-23 21:17:00 -0700

    added js practic exercises

---
+++ Coding Practice/js-practice.js
@@ -0,0 +1,15 @@
+let rep = 1;
+while (rep < 11){
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++
+};
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+
+while (dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end...`);
+
+};
