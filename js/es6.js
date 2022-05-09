110d9975a673123dc505d14458438a7ff71790b5
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-09 15:40:39 -0400

    completed first 5 to-do items

--- js/es6.js
+++ js/es6.js
@@ -1,5 +1,4 @@
-/*
- * Complete the TODO items below
+/* Complete the TODO items below
  */
 const users = [
   {
@@ -33,23 +32,23 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'Michael Kestler';
+const email = 'michaal.s.kestler@gmail.com';
+const languages = ['javascript', 'html', 'css'];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
+users.forEach((user) => {
   return emails.push(user.email);
 });
 users.forEach(function(user) {
@@ -57,7 +56,7 @@ users.forEach(function(user) {
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
@@ -80,3 +79,46 @@ developers.forEach(function (developer) {
   list += '<li>' + developer + '</li>';
 });
 list += '</ul>';
+
+
+//let and const
+// var - globally scoped
+// const - block scoped --> constant, you cannot change its value
+// cannot be re-assigned
+//let - block scoped but you can change it
+//without block scope
+
+// if (true) {
+//   let myName = 'codeup';
+//   console.log('after initial assignment scope: ', myName)
+//   myName = 'Michael'
+//   console.log('after re-assigning inside scope: ', myName)
+// }
+//
+// console.log(myName);
+//
+// function addAges(a,b){
+//   if(a>b) {
+//     let newAge = a + b;
+//     return newAge;
+//   } else {
+//     let otherAge = a+20;
+//     return otherAge;
+//   }
+// }
+//
+// addAges(6, 9);
+
+// let keywords have access outward inward but not inward outward
+
+//***** let and const are common interview question
+
+//template literals. back tics are used to initiate template literal. wrap variable in ${} and then type as you
+// would normally
+
+
+// for ... of - is a new way of looping - kind of shorthand for a 'for loop'
+
+//arrow functions - shortcut for writing functions
+
+
