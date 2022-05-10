88d64b0b3ad0f6e463a4f139397b42cc699fd401
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-10 16:08:31 -0400

    created promises js and html files

---
+++ js/promises-curriculum-notes.js
@@ -0,0 +1,21 @@
+fetch('https://api.github.com/users')
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
+
+const myPromise = fetch('https://api.github.com/users');
+myPromise.then(response => console.log(response));
+myPromise.catch(error => console.error(error));
+
+
+const githubPromise = fetch('https://api.github.com/repositories');
+const bitbucketPromise = fetch('https://api.bitbucket.org/2.0/repositories');
+
+Promise.all([githubPromise, bitbucketPromise])
+    .then(function(data) {
+        // here data is an array of the resolved values from each promise
+        // we can now do something with both pieces of data
+    })
+    .catch(function(error) {
+        // handle errors
+    });
