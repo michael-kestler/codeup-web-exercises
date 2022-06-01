838907b94ac0f2678c004d2e8b64886adbc707bd
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-31 18:48:56 -0700

    completed second task of promises js exercise. created wait function. promies exercise complete

--- js/promises.js
+++ js/promises.js
@@ -15,7 +15,7 @@ console.log(getLastCommit('mojombo'));
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
+    }
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
