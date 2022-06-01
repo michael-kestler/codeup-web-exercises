33a5a3a41e831e3bb2900d371eb0b99279fa08e9
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-31 19:14:06 -0700

    updated promises exercise html

--- js/promises.js
+++ js/promises.js
@@ -17,5 +17,5 @@ function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(1000).then(() => alert('You\'ll see this after 1 second'));
+wait(3000).then(() => alert('You\'ll see this after 3 seconds'));
