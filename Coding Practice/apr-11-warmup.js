2aea407cdd6dc5c6976cc5ec8332fd7e363d98c7
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-24 22:04:04 -0700

    wrote code to print the prime numbers less than or equal to 50

--- Coding Practice/apr-11-warmup.js
+++ Coding Practice/apr-11-warmup.js
@@ -2,9 +2,31 @@
 // Recommend starting your loop at 1 and ending your loop once you've calculated 50 primes.
 // https://en.wikipedia.org/wiki/Prime_number
 
-for (let i = 2; i < 50; i++) {
-    console.log(i % )
 
+
+let i = 0;
+let num = 0;
+
+for (i = 1; i<=50; i++){
+    let counter = 0;
+    for (num = i-1; num>=2; num--){
+        if(i%num===0){
+            counter = counter + 1;
+        }
+    }
+    if (counter === 0){
+            console.log(i);
+    }
+}
+
+
+
+// while
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 1 === 0){
+//         console.log(i);
+//     }
+// }
 
     // if (i % j == 0) {
     //     flag = 1;
