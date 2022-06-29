a44b5c17090d519ef7b5d1a162c79bf915e6c3ad
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-06-28 20:34:05 -0700

    added another solution to vowels and consonants exercise

--- Coding Practice/apr-19-warmup.js
+++ Coding Practice/apr-19-warmup.js
@@ -73,3 +73,20 @@ function vowelsAndConsonants (s){
 
 
 vowelsAndConsonants("Javascript");
+
+
+//another solution
+function vowelsAndConsonants2(s) {
+    const vowels = [],
+    consonants = [];
+
+    for(const c of s){
+        if('aeiou'.includes(c)){
+            vowels.push(c);
+        }else{
+            consonants.push(c);
+        }
+    }
+    console.log(vowels.join("\n"));
+    console.log(consonants.join("\n"));
+}
