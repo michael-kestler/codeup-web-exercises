b67e6ae99e844e53e4d234ad5115f3dcf2c7edde
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-07-01 22:13:16 -0700

    created function to calculate total cost of meal after tax and tip are calculated

--- Coding Practice/all-warmup-questions.js
+++ Coding Practice/all-warmup-questions.js
@@ -236,6 +236,20 @@ weirdChecker(17)
 
 
 
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+
+    let tip = meal_cost/100 *tip_percent;
+    let tax = tax_percent/100 * meal_cost;
+
+    let total_cost = Math.round(meal_cost + tip + tax);
+    console.log(total_cost);
+}
+
+
+
+
+
 
 
 
